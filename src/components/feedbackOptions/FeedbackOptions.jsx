8927b6a07b1c5aa166704823da4bf87343291c11
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-07-11 08:15:35 +0300

    vfvfv

--- src/components/feedbackOptions/FeedbackOptions.jsx
+++ src/components/feedbackOptions/FeedbackOptions.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import css from 'components/feedbackOptions/FeedbackOptions.module.css';
 export const FeedbackOptions = ({options,onLeaveFeedback})=>{
    return <ul className={css.btnlist}>
 {options.map(op =>{return <li key={op} className={css.btnlistitem}><button type='button' className={css.btn} onClick={onLeaveFeedback}>{op}</button></li>} )}
 </ul>
 }
-// FeedbackOptions.propTypes = {}
+FeedbackOptions.propTypes = {
+   options: PropTypes.object.isRequired,
+   onLeaveFeedback : PropTypes.func.isRequired,
+}
